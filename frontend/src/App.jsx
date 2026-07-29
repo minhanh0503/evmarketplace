2b30f901920f524174a5e2cea8b70465ede67332
minhanh0503 <nguyenminhanh290503@gmail.com>
2026-07-28 22:24:27 -0400

    added details page for the car catalog

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
+import CarDetails from "./pages/CarDetails";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/vehicles/:id" element={<CarDetails />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/checkout" element={<Checkout />} />
     </Routes>
