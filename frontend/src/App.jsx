ce36fcf33417cf5f409f85643c54407fcd7e28bf
Matin <matinpaki83@gmail.com>
2026-07-29 09:22:21 -0400

    merge App.jsx with cardetails and chatbot widget

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/vehicles/:id" element={<CarDetails />} />
-      <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/test-drive/:vehicleId" element={<TestDrive />} />
       </Routes>
